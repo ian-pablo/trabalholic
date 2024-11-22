0b3c625138538b5af40edd3dfa25d24ca13d931d
ian-pablo <167139972+ian-pablo@users.noreply.github.com>
2024-11-21 22:00:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function abremenu() {
+    var n = document.getElementById("nav")
+
+    if (n.style.display == "flex") {
+        n.style.display = "none";
+    } else {
+        n.style.display = "flex";
+    }
+}
+
+function fechamenu() {
+    var f = document.getElementById("nav")
+
+    if (f.style.display == "flex") {
+        f.style.display = "none";
+    } else {
+        f.style.display = "flex";
+    }
+}
